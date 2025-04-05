454f8fc183d7e8074bba765d304585eaf0b37749
ivanleonov <leonovivan001@gmail.com>
2025-04-05 20:52:34 +0400

    Задание выполнено

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -2,8 +2,53 @@ fun main () {
 
     //1. В этом же проекте создай файл с именем Task2.kt, а в нем класс Task2. (тут не понял как внутри файла создать класс)
 
-    val income: Double =
+    val income: Double = 5_000_000.00
+    var taxRate = 1
+    var tax: Double
 
+    println("Выберите название организации:")
+    println("1 - Сбербанк")
+    println("2 - ГосМедицина")
+    println("3 - Автоваз")
+    var industry = readln().toString()
+    when (industry) {
+
+        "1" -> {
+            when {
+                income <= 249999.0 -> taxRate = 10
+                income in 250000.0..499999.0 -> taxRate = 15
+                income >= 500000.0 -> taxRate = 18
+
+            }
+            industry = "Сбербанк"
+        }
+
+        "2" -> {
+            when {
+                income <= 399999.0 -> taxRate = 7
+                income in 400000.0..899999.0 -> taxRate = 12
+                income >= 900000.0 -> taxRate = 15
+            }
+            industry = "ГосМедицина"
+        }
+
+        "3"-> {
+            when {
+                income <= 99000.0 -> taxRate = 9
+                income in 100000.0..299999.0 -> taxRate = 13
+                income >= 300000.0 -> taxRate = 17
+            }
+            industry = "Автоваз"
+        }
+        else -> {
+            println("Ошибка")
+            return
+        }
+    }
+
+
+    tax = (income*taxRate)/100
+    println("Налог на сумму $income рублей для отрасли \"$industry\" составляет $tax р. Это составляет $taxRate процентов")
 
 
 
