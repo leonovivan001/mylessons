7fc57e8bd62e98282f80480d8c10ed224d3ba64b
ivanleonov <leonovivan001@gmail.com>
2025-04-10 22:19:34 +0400

    .

---
+++ loops/Task2.kt
@@ -0,0 +1,19 @@
+fun main () {
+
+    var numbers = IntArray(10)
+    var summOfNumbers: Int = 0
+
+    for (i in numbers.indices) numbers[i] = (1..99).random()
+    for (a in numbers) println(a)
+
+    for (i in numbers) {
+        summOfNumbers = summOfNumbers + i
+    }
+println("\nСумма всех элементов массива равна $summOfNumbers")
+
+    var everage = summOfNumbers / numbers.size
+    println("Среднее значение элементов массива: $everage")
+
+
+
+}
