52863c79bae6f67d152b5122f98ee70bb93f9a1d
ivanleonov <leonovivan001@gmail.com>
2025-04-16 01:41:32 +0400

    .

---
+++ strings/Task1.kt
@@ -0,0 +1,29 @@
+import java.util.*
+
+fun main () {
+
+//    var word = ("11dfdf 22dfdf 33dfghdgfhgdh")
+//    var i2 = arrayOf("Ваня", "Саша", "Петя", "Слава", "Сергей", "Поликарп")
+//
+//    println(word.split(" ").takeLast(1))
+////    println(i2.takeLast(3))
+//
+//    val kitty = "mur@zik@gmail.com"
+//    println(kitty.substringBefore("@")) // murzik
+////    println(word.substringBefore("ghdg"))
+
+//    val string = "090d32020"
+
+    val string = "Кит Кишка"
+    val result = string.replace("Кишка", "Синий")
+
+    println(result)
+
+
+
+
+
+
+
+
+}
