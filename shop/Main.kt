28e22e11a2554994586ec6d803cc054014ad6018
ivanleonov <leonovivan001@gmail.com>
2025-08-31 20:49:53 +0200

    .

--- shop/Main.kt
+++ shop/Main.kt
@@ -6,14 +6,3 @@ fun main() {
     printInfo(42) // Число
     printInfo("1")
 }
-open class Animal {
-    open fun speak() {
-        println("Какое-то животное подаёт звук")
-    }
-}
-
-interface Animal2 {
-    fun speak() {
-        println("Какое-то животное подаёт звук")
-    }
-}
